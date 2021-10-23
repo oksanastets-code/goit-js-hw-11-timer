dd321183511f5cb30c0f499b7b9aaf6c72ab87db
Oksana Stets <oksanastets73@gmail.com>
2021-10-23 17:41:53 +0300

    adds files

---
+++ timer.js
@@ -0,0 +1,61 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+  value: document.querySelectorAll(".value"),
+};
+
+class CountdownTimer {
+  constructor({ onTick, selector, targetDate }) {
+    this.intervalId = null;
+    this.onTick = onTick;
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = this.targetDate - currentDate;
+      if (deltaTime < 0) {
+        return this.stop();
+      }
+      const time = this.getTimeComponents(deltaTime);
+      this.onTick(time);
+    }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+const countdownTimer = new CountdownTimer({
+  onTick: updateClockface,
+  selector: "#timer-1",
+  targetDate: new Date("Oct 30, 2021"),
+});
+
+countdownTimer.start();
+
+function updateClockface({ days, hours, mins, secs }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
